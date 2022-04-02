ef4031fae17e9c8bd3e21e9726edd608d6eb2e17
nwe3er <77739655+nwe3er@users.noreply.github.com>
2022-04-02 00:39:45 -0500

    updated to Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ import java.util.*;
 // Main class
 public class Main {
     public static void main(String[] args) {
+        // list to store the kb
         ArrayList<String[]> kb = new ArrayList<>();
+        // path of the argument
         String path1 = args[0];
+        // clause to be proved
         String[] testClause;
+        // counter
+        int clauseCount = 1;
 
         // open path1 file to get the kb and the clause to test
         try {
@@ -32,18 +37,38 @@ public class Main {
             e.printStackTrace();
         }
 
-        // assign the clause to prove
+        // assign the clause needed to be proved
         testClause = (kb.get(kb.size()-1));
-        // remove the last line from the kb
+        // remove the last element from the kb
         kb.remove(kb.size()-1);
 
-        // loop to negate the clause to prove
+        // loop to negate the clause to solve
         for(int i = 0; i < testClause.length; i++) {
             // if ~ is included, remove it
             if (testClause[i].contains("~")) {
                 testClause[i] = testClause[i].replace("~", "");
             }
+            // else add ~ to the element
+            else {
+                testClause[i] = testClause[i].replace(testClause[i], "~" + testClause[i]);
+            }
         }
+
+        // loop the kb and print the clauses to the console
+        for (String[] strings : kb) {
+            System.out.println(clauseCount + ". " + String.join(" ", strings) + " { }");
+            clauseCount++;
+        }
+
+        // loop through test clause, add negation to kb and print the clause to the console
+        for (String s : testClause) {
+            String[] temp = new String[1];
+            temp[0] = s;
+            kb.add(temp);
+            System.out.println(clauseCount + ". " + String.join(" ", kb.get(kb.size() - 1)) + " { }");
+            clauseCount++;
+        }
+
 
 
     }

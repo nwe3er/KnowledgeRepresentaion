9dc6e26766d3acca21adef0ca0b90473bb6a4477
nwe3er <77739655+nwe3er@users.noreply.github.com>
2022-04-01 23:21:21 -0500

    updated to Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,48 @@ import java.util.*;
 // Main class
 public class Main {
     public static void main(String[] args) {
-        int clauseInteger = 1;
-        ArrayList<String[]> clauses = new ArrayList<>();
-        String kb = args[0];
-        ArrayList<String[]> testClause = new ArrayList<>();
+        ArrayList<String[]> kb = new ArrayList<>();
+        String path1 = args[0];
+        String[] testClause;
 
+        // open path1 file to get the kb and the clause to test
         try {
-            File file1 = new File(kb);
+            // open scanner on .kb file
+            File file1 = new File(path1);
             Scanner scanner =  new Scanner(file1);
 
+            // loop through the file adding elements to the kb list
             while(scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] tempList = line.split(" ");
 
-                clauses.add(tempList);
+                kb.add(tempList);
             }
 
+            // close scanner
             scanner.close();
 
-            testClause.add(clauses.get(clauses.size()-1));
-            clauses.remove(clauses.size()-1);
-
+         // print the error message if the file could not be opened
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred, File could not be opened.");
             e.printStackTrace();
         }
 
+        // assign the clause to prove
+        testClause = (kb.get(kb.size()-1));
+        // remove the last line from the kb
+        kb.remove(kb.size()-1);
+
+        // loop to negate the clause to prove
+        for(int i = 0; i < testClause.length; i++) {
+            // if ~ is included, remove it
+            if (testClause[i].contains("~")) {
+                testClause[i] = testClause[i].replace("~", "");
+            }
+        }
+
 
     }
+
+
 }
